b894bfeed2ec800b5a113a6a486dc84aa64eaf90
root <root@mailproc.wrtdesign.com>
2011-12-05 11:17:24 -0500

    proper zip download - adjusted for local server instead of using FTP

--- download_zip.php
+++ download_zip.php
@@ -1,33 +1,29 @@
 <?php
-/* $username = "dropped"; */
-/* $password = "files"; */
-/* $server = "ftp.wrtdesign.com"; */
-
-$files = $_GET['fa'];
-$path = $_GET['p'];
-$fullpath = '/dropdir/' . $path;
-//$files = array($argv[1]);
+//$files = $_GET['fa'];
+//$path = $_GET['p'];
+$files = array('TEST.DWG');
+$path = 'f49ec93d00d8fc99c9f4905806abe61389bd4cb0';
+$fullpath = '/dropdir/' . $path . '/';
 
 $ra_zip = tempnam("tmp", "zip");
 
 $zip = new ZipArchive();
 $zip->open($ra_zip, ZipArchive::OVERWRITE);
 foreach ($files as $file) {
-    $zip->addFile($fullpath . '/' . $file, $file);
+    $zip->addFile($fullpath . $file, $file);
 }
 $zip->close();
 
 $fsize = filesize($ra_zip);
-//print $fsize;
 
 //header("Pragma: public");
 //header("Expires: 0");
 //header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 //header("Cache-Control: public");
-//header("Content-Description: File Transfer"); 
+//header("Content-Description: File Transfer");
 header("Content-Type: application/zip");
 header("Content-Disposition: attachment; filename=\"RemovedAttachments.zip\"");
-//header("Content-Transfer-Encoding: binary"); 
+//header("Content-Transfer-Encoding: binary");
 header("Content-Length: " . $fsize);
 
 ob_clean();
@@ -36,3 +32,4 @@ flush();
 readfile($ra_zip);
 unlink($ra_zip);
 ?>
+
