49e80f14f01764676a48cc5ed0ee133e0b494896
root <root@mailproc-test.dev.wrtdesign.com>
2012-08-14 09:12:27 -0400

    more testing

--- EARS_db.py
+++ EARS_db.py
@@ -1,4 +1,7 @@
 from sqlalchemy import Table, Column, Integer, String, DateTime, MetaData, ForeignKey
+
+from sqlalchemy.orm import relationship, backref
+
 from sqlalchemy.dialects.mysql import \
         BIGINT, BINARY, BIT, BLOB, BOOLEAN, CHAR, DATE, \
         DATETIME, DECIMAL, DECIMAL, DOUBLE, ENUM, FLOAT, INTEGER, \
@@ -14,11 +17,14 @@ class Attachment(Base):
     __tablename__ = 'attachment'
 
     id = Column(Integer, primary_key=True)
-    filename = Column(String)
+    filename = Column(VARCHAR(100))
     filesize = Column(Integer)
-    fileHash = Column(String)
+    fileHash = Column(VARCHAR(100))
     file = Column(BLOB)
     dateReceived = Column(DateTime)
+    
+    message_id = Column(Integer, ForeignKey('message.id'))
+
 
     def __init__(self, id, filename, filesize, fileHash, dateReceived):
         self.id = id
@@ -31,61 +37,73 @@ class Attachment(Base):
         return "<Attachment('%s','%s','%s', '%s'>" % (self.filename, self.filesize, self.fileHash, self.dateReceived)
 
 
-class Attachment_Link(Base):
-    __tablename__ = "att_link"
+#class Attachment_Link(Base):
+#    __tablename__ = "att_link"
 
-    id = Column(Integer, primary_key=True)
-    fileID = Column(Integer)
-    msgID = Column(Integer)
+#    id = Column(Integer, primary_key=True)
+#    fileID = Column(Integer, ForeignKey('attachment.id'))
+#    msgID = Column(Integer, ForeignKey('message.id'))
 
-    def __init__(self, fileID, msgID):
-        self.fileID = fileID
-        self.msgID = msgID
+#    def __init__(self, fileID, msgID):
+#        self.fileID = fileID
+#        self.msgID = msgID
 
 class Message(Base):
     __tablename__ = "message"
 
     id = Column(Integer, primary_key=True)
-    subject = Column(String)
-    headers = Column(String)
-    body = Column(String)
+    subject = Column(VARCHAR(100))
+    headers = Column(VARCHAR(100))
+    body = Column(VARCHAR(100))
     dateReceived = Column(DateTime)
     raw_original = Column(LONGTEXT)
 
+#    sender_id = Column(Integer, ForeignKey('sender.id'))
+#    recipient_id = Column(Integer, ForeignKey('recipient.id'))
+#    attachment_id = Column(Integer, ForeignKey('attachment.id'))
+
+    sender = relationship('Sender', backref=backref('message', order_by=id))
+    recipients = relationship('Recipient', backref=backref('message', order_by=id))
+    attachments = relationship('Attachment', backref=backref('message', order_by=id))
+
+
     def __init__(self, subject, headers, body, dateReceived, raw_original):
-        self.subject = subjet
+        self.subject = subject
         self.headers = headers
         self.body = body
         self.dateReceived = dateReceived
         self.raw_original = raw_original
 
-class Message_Recipient_Link(Base):
-    __tablename__ = 'mr_link'
+# class Message_Recipient_Link(Base):
+#     __tablename__ = 'mr_link'
 
-    id = Column(Integer, primary_key=True)
-    recipID = Column(Integer)
-    msgID = Column(Integer)
+#     id = Column(Integer, primary_key=True)
+#     recipID = Column(Integer)
+#     msgID = Column(Integer)
 
-    def __init__(self, recipID, msgID):
-        self.recipID = recipID
-        self.msgID = msgID
+#     def __init__(self, recipID, msgID):
+#         self.recipID = recipID
+#         self.msgID = msgID
 
-class Message_Sender_Link(Base):
-    __tablename__ = 'ms_link'
+# class Message_Sender_Link(Base):
+#     __tablename__ = 'ms_link'
 
-    id = Column(Integer, primary_key=True)
-    senderID = Column(Integer)
-    msgID = Column(Integer)
+#     id = Column(Integer, primary_key=True)
+#     senderID = Column(Integer)
+#     msgID = Column(Integer)
 
-    def __init__(self, senderID, msgID):
-        self.senderID = senderID
-        self.msgID = msgID
+#     def __init__(self, senderID, msgID):
+#         self.senderID = senderID
+#         self.msgID = msgID
 
 class Recipient(Base):
     __tablename__ = 'recipient'
 
     id = Column(Integer, primary_key=True)
-    emailAddress = Column(String)
+    emailAddress = Column(VARCHAR(100))
+
+    message_id = Column(Integer, ForeignKey('message.id'))
+    
 
     def __init__(self, emailAddress):
         self.emailAddress = emailAddress
@@ -94,7 +112,14 @@ class Sender(Base):
     __tablename__ = 'sender'
 
     id = Column(Integer, primary_key=True)
-    emailAddress = Column(String)
-
+    emailAddress = Column(VARCHAR(100))
+    
+    message_id = Column(Integer, ForeignKey('message.id'))
+    
     def __init__(self, emailAddress):
         self.emailAddress = emailAddress
+
+    def __repr__(self):
+        return self.emailAddress
+
+        
--- removeFromDB.py
+++ removeFromDB.py
@@ -1,12 +1,15 @@
 #/usr/bin/env python
+import EARS_db
 
 from datetime import date, timedelta
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
 from EARS_db import Attachment, Message, Recipient, Sender
-from EARS_db import Attachment_Link as att_link
+#from EARS_db import Attachment_Link as att_link
+
 
 daysago = date.today() - timedelta(days=30)
 
@@ -14,19 +17,18 @@ class oldItems():
     def __init__(self):
         Session = sessionmaker()
 
-        engine = create_engine('mysql+mysqldb://root:python@python.dev.wrtdesign.com/EARS')
+        engine = create_engine('mysql+mysqldb://root:python@python.dev.wrtdesign.com/EARS', echo=True)
         Session.configure(bind=engine)
         self.session = Session()
-        
+
+        EARS_db.Base.metadata.create_all(engine)
+
 
 def main():
     items = oldItems()
 
-    query = items.session.query(Attachment).filter(Attachment.dateReceived < daysago)
-    x =query.all()
-    for i in x:
-        print i
-    
+#    query = items.session.query(Attachment).filter(Attachment.dateReceived < daysago)
+#    query.all()
 
 
 if __name__ == '__main__':
